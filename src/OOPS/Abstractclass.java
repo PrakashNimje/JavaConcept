b09c130c1ac4b8027cd762bdea4826991de2bf74
PrakashNimje <prakashnimje800@gmail.com>
2021-08-31 18:00:51 +0530

    This is My Second Commit

---
+++ src/OOPS/Abstractclass.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+interface FirstInterface {
+	  public void myMethod(); // interface method
+	}
+
+	interface SecondInterface {
+	  public void myOtherMethod(); // interface method
+	}
+
+	class DemoClass implements FirstInterface, SecondInterface {
+	  public void myMethod() {
+	    System.out.println("Some text..");
+	  }
+	  public void myOtherMethod() {
+	    System.out.println("Some other text...");
+	  }
+	}
+
+	class Abstractclass {
+	  public static void main(String[] args) {
+	    DemoClass myObj = new DemoClass();
+	    myObj.myMethod();
+	    myObj.myOtherMethod();
+	  }
+	}
+
+
+
+
